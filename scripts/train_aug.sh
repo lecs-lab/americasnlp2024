a8536fad3546ca4e0f2d0964c35bd02337194305
michaelpginn <milespergallon271@gmail.com>
2024-04-08 12:13:15 -0600

    Fix aug script

--- scripts/train_aug.sh
+++ scripts/train_aug.sh
@@ -20,56 +20,58 @@ module load anaconda
 conda activate americasnlp2024
 cd "/projects/migi8081/americasnlp2024"
 
-arch='attentive_lstm'
 method="$1"
 
 mkdir data/temp
 
 for lang in bribri maya guarani 
 do
-  # Concat the train and augmented datasets
-  # If you need multiple aug dataset, use `conjoin_dataframes` to create a joint augmented dataset and then pass that dataset here
-  python scripts/conjoin_dataframes.py data/yoyodyne/$lang-train.tsv data/augmented/$lang-$method.tsv data/temp/$lang-train+$method.tsv
+  for arch in attentive_lstm pointer_generator_lstm
+  do
+    # Concat the train and augmented datasets
+    # If you need multiple aug dataset, use `conjoin_dataframes` to create a joint augmented dataset and then pass that dataset here
+    python scripts/conjoin_dataframes.py data/yoyodyne/$lang-train.tsv data/augmented/$lang-$method.tsv data/temp/$lang-train+$method.tsv
 
-  yoyodyne-train \
-    --experiment 2024americasnlp-$lang \
-    --model_dir models/aug \
-    --train data/temp/$lang-train+$method.tsv \
-    --val data/yoyodyne/$lang-dev.tsv \
-    --features_col 3 \
-    --arch $arch \
-    --batch_size 32 \
-    --max_epochs 100 \
-    --scheduler lineardecay \
-    --log_wandb \
-    --seed 0
-      # --accelerator gpu \
+    yoyodyne-train \
+      --experiment 2024americasnlp-$lang \
+      --model_dir models/aug \
+      --train data/temp/$lang-train+$method.tsv \
+      --val data/yoyodyne/$lang-dev.tsv \
+      --features_col 3 \
+      --arch $arch \
+      --features_encoder_arch linear \
+      --batch_size 32 \
+      --max_epochs 100 \
+      --scheduler lineardecay \
+      --log_wandb \
+      --seed 0 \
+      --accelerator gpu \
 
 
-  ckpt_file=(./models/aug/2024americasnlp-$lang/version_0/checkpoints/*.ckpt)
-  ckpt_file=${ckpt_file[0]}
+    ckpt_file=(./models/aug/2024americasnlp-$lang/version_0/checkpoints/*.ckpt)
+    ckpt_file=${ckpt_file[0]}
 
-  echo Loading checkpoint file from $ckpt_file
+    echo Loading checkpoint file from $ckpt_file
 
-  yoyodyne-predict \
-    --model_dir ./models/aug \
-    --experiment 2024americasnlp-$lang \
-    --checkpoint "$ckpt_file" \
-    --predict "data/yoyodyne/$lang-dev.tsv" \
-    --output "./preds/aug/$method/$arch-$lang.tsv" \
-    --features_col 3 \
-    --target_col 0 \
-    --arch $arch \
-    --no_save_best \
-    --accelerator gpu \
+    yoyodyne-predict \
+      --model_dir ./models/aug \
+      --experiment 2024americasnlp-$lang \
+      --checkpoint "$ckpt_file" \
+      --predict "data/yoyodyne/$lang-dev.tsv" \
+      --output "./preds/aug/$method/$arch-$lang.tsv" \
+      --features_col 3 \
+      --target_col 0 \
+      --arch $arch \
+      --accelerator gpu \
 
-  # Move the folder so we only ever have one numbered version
-  mv ./models/aug/2024americasnlp-$lang/version_0 ./models/aug/2024americasnlp-$lang/$arch
+    # Move the folder so we only ever have one numbered version
+    mv ./models/aug/2024americasnlp-$lang/version_0 ./models/aug/2024americasnlp-$lang/$arch
 
-  python ./scripts/copy_preds.py "./preds/aug/$method/$arch-$lang.tsv" "data/yoyodyne/$lang-dev.tsv"
+    python ./scripts/copy_preds.py "./preds/aug/$method/$arch-$lang.tsv" "data/yoyodyne/$lang-dev.tsv"
 
-  python ./americasnlp2024/ST2_EducationalMaterials/baseline/evaluate.py "./preds/aug/$method/$arch-$lang.tsv" > "./preds/aug/$method/$arch-$lang-eval.out"
+    python ./americasnlp2024/ST2_EducationalMaterials/baseline/evaluate.py "./preds/aug/$method/$arch-$lang.tsv" > "./preds/aug/$method/$arch-$lang-eval.out"
 
-  rm data/temp/*.tsv
+    rm data/temp/*.tsv
+  done
 done
 
