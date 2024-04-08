9d848fa44dc2d554372207e3face2a042e255a59
michaelpginn <milespergallon271@gmail.com>
2024-04-07 20:39:26 -0600

    Add missing flag

--- scripts/train_aug.sh
+++ scripts/train_aug.sh
@@ -28,7 +28,7 @@ mkdir data/temp
 for lang in bribri maya guarani 
 do
   # Concat the train and augmented datasets
-  # TODO: Add support for multiple aug datasets
+  # If you need multiple aug dataset, use `conjoin_dataframes` to create a joint augmented dataset and then pass that dataset here
   python scripts/conjoin_dataframes.py data/yoyodyne/$lang-train.tsv data/augmented/$lang-$method.tsv data/temp/$lang-train+$method.tsv
 
   yoyodyne-train \
@@ -59,8 +59,9 @@ do
     --output "./preds/aug/$method/$arch-$lang.tsv" \
     --features_col 3 \
     --target_col 0 \
-    --arch $arch
-    # --accelerator gpu \
+    --arch $arch \
+    --no_save_best \
+    --accelerator gpu \
 
   # Move the folder so we only ever have one numbered version
   mv ./models/aug/2024americasnlp-$lang/version_0 ./models/aug/2024americasnlp-$lang/$arch
