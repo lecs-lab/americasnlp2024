d831f420dfe0e18769fd1e59f4a9e28c1d1481f8
michaelpginn <milespergallon271@gmail.com>
2024-04-10 13:39:30 -0600

    Add test data and script for final predictions

---
+++ scripts/train_final_and_predict_test.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#SBATCH --nodes=1           # Number of requested nodes
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=4          # Number of requested cores
+#SBATCH --mem=32G
+#SBATCH --time=7-00:00:00          # Max walltime              
+#SBATCH --qos=blanca-curc-gpu
+#SBATCH --partition=blanca-curc-gpu
+#SBATCH --account=blanca-curc-gpu
+#SBATCH --out=americasnlp.%j.out      # Output file name
+#SBATCH --error=americasnlp.%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=michael.ginn@colorado.edu
+
+# purge all existing modules
+module purge
+# Load the python module
+module load anaconda
+# Run Python Script
+conda activate americasnlp2024
+cd "/projects/migi8081/americasnlp2024"
+
+lang="$1"
+arch="$2"
+method="$3"
+
+mkdir data/temp
+
+# Concat the train/dev and augmented datasets
+# If you need multiple aug dataset, use `conjoin_dataframes` to create a joint augmented dataset and then pass that dataset here
+python scripts/conjoin_dataframes.py data/yoyodyne/$lang-train-dev.tsv data/test-augmented/$lang-$method.tsv data/temp/$lang-train-dev+$method.tsv
+
+yoyodyne-train \
+  --experiment 2024americasnlp-$lang-final \
+  --model_dir models/test-aug/$method \
+  --train data/temp/$lang-train-dev+$method.tsv \
+  --val data/yoyodyne/$lang-dev.tsv \
+  --features_col 3 \
+  --arch $arch \
+  --features_encoder_arch linear \
+  --batch_size 32 \
+  --max_epochs 100 \
+  --scheduler lineardecay \
+  --log_wandb \
+  --seed 0 \
+  --no_save_best \
+  --accelerator gpu \
+
+
+ckpt_file=(./models/test-aug/$method/2024americasnlp-$lang-final/version_0/checkpoints/*.ckpt)
+ckpt_file=${ckpt_file[0]}
+
+echo Loading checkpoint file from $ckpt_file
+
+yoyodyne-predict \
+  --model_dir ./models/test-aug/$method \
+  --experiment 2024americasnlp-$lang-final \
+  --checkpoint "$ckpt_file" \
+  --predict "data/yoyodyne/$lang-test.tsv" \
+  --output "./test-preds/aug/$method/$arch-$lang.tsv" \
+  --features_col 2 \
+  --target_col 0 \
+  --arch $arch \
+  --accelerator gpu \
+
+# Move the folder so we only ever have one numbered version
+mv ./models/test-aug/$method/2024americasnlp-$lang-final/version_0 ./models/test-aug/$method/2024americasnlp-$lang-final/$arch
+
+python ./scripts/copy_preds.py "./test-preds/aug/$method/$arch-$lang.tsv" "data/yoyodyne/$lang-test.tsv"
+
+rm data/temp/*.tsv
